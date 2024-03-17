88e5dd54e7a41c4b53d0efa00568bea574c99e0c
akash <akashsonkamble199@gmail.com>
2024-03-17 19:03:13 +0530

    feat: Import Hero component into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+
+import "./App.css";
 
 function App() {
   return <>
     <Navbar />
+    <div className="container">
+      <Hero />
+    </div>
   </>;
 }
 
